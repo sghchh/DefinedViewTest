ca172e43c8442969590adf320b63069f67c34219
sgh <sgh2810@163.com>
2017-12-24 20:10:21 +0800

    安卓设置更改的view的实现

--- app/src/main/java/com/example/as/definedviewtest/MainActivity.java
+++ app/src/main/java/com/example/as/definedviewtest/MainActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private MyCircleImageView imageView;
+    private StateView state;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView=findViewById(R.id.circle);
+        state=(StateView)findViewById(R.id.state);
+        state.initListener();
     }
 }
